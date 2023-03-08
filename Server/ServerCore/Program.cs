01cc502e711a492f827102d43cf36707615deea2
김승혁 <seunghyuk@nhn.com>
2023-03-08 12:55:18 +0900

    Spinlock sample

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -2,10 +2,58 @@
 
 namespace ServerCore;
 
+internal class SpinLock
+{
+    private volatile int _lock;
+
+    public void Acquire()
+    {
+        var expected = 0;
+        var desired = 1;
+        while (true)
+            if (Interlocked.CompareExchange(ref _lock, desired, expected) == expected)
+                break;
+    }
+
+    public void UnAcquire()
+    {
+        _lock = 0;
+    }
+}
+
 internal class Program
 {
+    private static SpinLock _spinLock = new();
+    private static int _num = 0;
+
+    private static void Thread1()
+    {
+        for (var i = 0; i < 10000000; i++)
+        {
+            _spinLock.Acquire();
+            _num += 1;
+            _spinLock.UnAcquire();
+        }
+    }
+
+    private static void Thread2()
+    {
+        for (var i = 0; i < 10000000; i++)
+        {
+            _spinLock.Acquire();
+            _num -= 1;
+            _spinLock.UnAcquire();
+        }
+    }
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var t1 = new Task(Thread1);
+        var t2 = new Task(Thread2);
+        t1.Start();
+        t2.Start();
+        Task.WaitAll(t1, t2);
+
+        Console.WriteLine("Num = {0}", _num);
     }
 }
