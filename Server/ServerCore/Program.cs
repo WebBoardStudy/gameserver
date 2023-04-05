f493a934eee7c421126f58e330a563162fcc7855
김승혁 <seunghyuk@nhn.com>
2023-04-06 07:34:09 +0900

    ReadWriteLock 구현 추가

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -23,33 +23,50 @@ internal class SpinLock
 
 internal class Program
 {
-    private static SpinLock _spinLock = new();
-    private static int _num = 0;
+    //private static SpinLock _spinLock = new();
+    private static volatile int _num = 0;
+    private static ReadWriteLock _readWriteLock = new ReadWriteLock();
+    
+    // private static void Thread1()
+    // {
+    //     for (var i = 0; i < 10000000; i++)
+    //     {
+    //         _spinLock.Acquire();
+    //         _num += 1;
+    //         _spinLock.UnAcquire();
+    //     }
+    // }
 
-    private static void Thread1()
-    {
-        for (var i = 0; i < 10000000; i++)
-        {
-            _spinLock.Acquire();
-            _num += 1;
-            _spinLock.UnAcquire();
-        }
-    }
-
-    private static void Thread2()
-    {
-        for (var i = 0; i < 10000000; i++)
-        {
-            _spinLock.Acquire();
-            _num -= 1;
-            _spinLock.UnAcquire();
-        }
-    }
+    // private static void Thread2()
+    // {
+    //     for (var i = 0; i < 10000000; i++)
+    //     {
+    //         _spinLock.Acquire();
+    //         _num -= 1;
+    //         _spinLock.UnAcquire();
+    //     }
+    // }
 
     private static void Main(string[] args)
     {
-        var t1 = new Task(Thread1);
-        var t2 = new Task(Thread2);
+        var t1 = new Task(delegate
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                _readWriteLock.WriteLock();
+                _num++;
+                _readWriteLock.WriteUnlock();
+            }
+        });
+        var t2 = new Task(delegate
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                _readWriteLock.WriteLock();
+                _num--;
+                _readWriteLock.WriteUnlock();
+            }
+        });
         t1.Start();
         t2.Start();
         Task.WaitAll(t1, t2);
