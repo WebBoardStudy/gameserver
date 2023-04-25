5c7cd0884a9134156648ed36bd6af81555f5e7b6
kauri <shkim2568@gmail.com>
2023-04-25 19:19:22 +0900

    session #3

--- Server/ServerCore/Session.cs
+++ Server/ServerCore/Session.cs
@@ -7,17 +7,19 @@ namespace ServerCore;
 public class Session {
     private Socket _socket;
     private int _disconnected = 0;
-    private SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
     private Queue<byte[]> _sendQueue = new Queue<byte[]>();
-    private bool _sendPending = false;
     private object _sendLock = new object();
+    private List<ArraySegment<byte>> _pendingList = new List<ArraySegment<byte>>();
+    private SocketAsyncEventArgs _recvArgs = new SocketAsyncEventArgs();
+    private SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
+
 
     public void Start(Socket socket) {
         _socket = socket;
-        var recvArgs = new SocketAsyncEventArgs();
-        recvArgs.Completed += OnRecvCompleted;
-        recvArgs.SetBuffer(new byte[1024], 0, 1024);
-        RegisterRecv(recvArgs);
+
+        _recvArgs.Completed += OnRecvCompleted;
+        _recvArgs.SetBuffer(new byte[1024], 0, 1024);
+        RegisterRecv();
 
         _sendArgs.Completed += OnSendCompleted;
     }
@@ -38,9 +40,9 @@ public class Session {
         }
     }
 
-    private void RegisterRecv(SocketAsyncEventArgs args) {
-        var pending = _socket.ReceiveAsync(args);
-        if (pending == false) OnRecvCompleted(null, args);
+    private void RegisterRecv() {
+        var pending = _socket.ReceiveAsync(_recvArgs);
+        if (pending == false) OnRecvCompleted(null, _recvArgs);
     }
 
     public void Disconnect() {
@@ -53,17 +55,21 @@ public class Session {
 
         lock (_sendLock) {
             _sendQueue.Enqueue(sendBuffer);
-            if (_sendPending == false) {
+            if (_pendingList.Count == 0) {
                 RegisterSend();
             }
         }
     }
 
     private void RegisterSend() {
-        _sendPending = true;
 
-        byte[] sendBuffer = _sendQueue.Dequeue();
-        _sendArgs.SetBuffer(sendBuffer, 0, sendBuffer.Length);
+        while (_sendQueue.Count > 0) {
+            byte[] sendBuffer = _sendQueue.Dequeue();
+            _pendingList.Add(sendBuffer);
+        }
+
+        _sendArgs.BufferList = _pendingList;
+
         bool pending = _socket.SendAsync(_sendArgs);
         if (pending == false) {
             OnSendCompleted(null, _sendArgs);
@@ -74,13 +80,14 @@ public class Session {
         lock (_sendLock) {
             if (args.SocketError == SocketError.Success && args.BytesTransferred > 0) {
                 try {
+                    _sendArgs.BufferList = null;
+                    _pendingList.Clear();
+
+                    Console.WriteLine($"Transfered bytes: {args.BytesTransferred}");
+
                     if (_sendQueue.Count > 0) {
                         RegisterSend();
                     }
-                    else {
-                        _sendPending = false;
-                    }
-
                 }
                 catch (Exception ex) {
                     Console.WriteLine($"OnSendCompleted failed {ex}");
