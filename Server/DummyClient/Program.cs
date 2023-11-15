c79e3340fe9944024be1eaebd898b713c7830e21
kimsy <kimsy@nhn.com>
2023-11-15 10:35:49 +0900

    skill ai 소스코드 적용

--- Server/DummyClient/Program.cs
+++ Server/DummyClient/Program.cs
@@ -1,42 +1,43 @@
-﻿using System;
+﻿using ServerCore;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
-namespace DummyClient;
-
-internal class Program
+namespace DummyClient
 {
-    private static void Main(string[] args)
-    {
-        var hostName = Dns.GetHostName();
-        var ipHostEntry = Dns.GetHostEntry(hostName);
-        var remoteEp = new IPEndPoint(ipHostEntry.AddressList[0], 11000);
+	
 
-        var socket = new Socket(remoteEp.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// DNS (Domain Name System)
+			string host = Dns.GetHostName();
+			IPHostEntry ipHost = Dns.GetHostEntry(host);
+			IPAddress ipAddr = ipHost.AddressList[0];
+			IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-        try
-        {
-            socket.Connect(remoteEp);
+			Connector connector = new Connector();
 
-            Console.WriteLine($"Connect to {remoteEp.ToString()}");
+			connector.Connect(endPoint, 
+				() => { return SessionManager.Instance.Generate(); },
+				500);
 
-            var sendBuffer = Encoding.UTF8.GetBytes("hello! 안녕! 반가워~");
-            var sendBytes = socket.Send(sendBuffer);
+			while (true)
+			{
+				try
+				{
+					SessionManager.Instance.SendForEach();
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e.ToString());
+				}
 
-            var recvBuffer = new byte[1024];
-            var recvBytes = socket.Receive(recvBuffer);
-
-            var recvString = Encoding.UTF8.GetString(recvBuffer, 0, recvBytes);
-            Console.WriteLine($"[From Server] : {recvString}");
-
-            socket.Shutdown(SocketShutdown.Both);
-            socket.Close();
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            throw;
-        }
-    }
+				Thread.Sleep(250);
+			}
+		}
+	}
 }
--- Server/ServerCore/Listener.cs
+++ Server/ServerCore/Listener.cs
@@ -1,21 +1,57 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
-namespace ServerCore;
-
-public class Listener
+namespace ServerCore
 {
-    private Socket? _listenSocket;
+	public class Listener
+	{
+		Socket _listenSocket;
+		Func<Session> _sessionFactory;
 
-    public void Init(IPEndPoint ipEndPoint)
-    {
-        _listenSocket = new Socket(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-        _listenSocket.Bind(ipEndPoint);
-        _listenSocket.Listen(10);
-    }
+		public void Init(IPEndPoint endPoint, Func<Session> sessionFactory, int register = 10, int backlog = 100)
+		{
+			_listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+			_sessionFactory += sessionFactory;
 
-    public Socket Accept()
-    {
-        return _listenSocket?.Accept()!;
-    }
+			// 문지기 교육
+			_listenSocket.Bind(endPoint);
+
+			// 영업 시작
+			// backlog : 최대 대기수
+			_listenSocket.Listen(backlog);
+
+			for (int i = 0; i < register; i++)
+			{
+				SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+				args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+				RegisterAccept(args);
+			}
+		}
+
+		void RegisterAccept(SocketAsyncEventArgs args)
+		{
+			args.AcceptSocket = null;
+
+			bool pending = _listenSocket.AcceptAsync(args);
+			if (pending == false)
+				OnAcceptCompleted(null, args);
+		}
+
+		void OnAcceptCompleted(object sender, SocketAsyncEventArgs args)
+		{
+			if (args.SocketError == SocketError.Success)
+			{
+				Session session = _sessionFactory.Invoke();
+				session.Start(args.AcceptSocket);
+				session.OnConnected(args.AcceptSocket.RemoteEndPoint);
+			}
+			else
+				Console.WriteLine(args.SocketError.ToString());
+
+			RegisterAccept(args);
+		}
+	}
 }
