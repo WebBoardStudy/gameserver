e8ac97535c24b3173d53aa5bdd1f978f8372715b
김승혁 <seunghyuk@nhn.com>
2023-12-13 07:58:05 +0900

    remove all

--- Server/PacketGenerator/GenPacket.bat
+++
@@ -1,6 +0,0 @@
-cd ./bin
-start PacketGenerator.exe ../PDL.xml
-xcopy GenPacket.cs "../../Server/Packet/GenPackets.cs" /Y /F
-xcopy GenPacket.cs "../../DummyClient/Packet/GenPackets.cs" /Y /F
-xcopy ServerPacketManager.cs "../../Server/Packet/PacketManager.cs" /Y /F
-xcopy ClientPacketManager.cs "../../DummyClient/Packet/PacketManager.cs" /Y /F
