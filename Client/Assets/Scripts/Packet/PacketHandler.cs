c493a6b7d46fd368e16ac281df02375041a953e3
김승혁 <seunghyuk@nhn.com>
2023-12-26 15:24:21 +0900

    서버연동 - 게임입장

--- Client/Assets/Scripts/Packet/PacketHandler.cs
+++ Client/Assets/Scripts/Packet/PacketHandler.cs
@@ -7,17 +7,45 @@ using UnityEngine;
 
 class PacketHandler
 {
-	public static void S_ChatHandler(PacketSession session, IMessage packet)
-	{
-		S_Chat chatPacket = packet as S_Chat;
-		ServerSession serverSession = session as ServerSession;
-
-		Debug.Log(chatPacket.Context);
-	}
-
 	public static void S_EnterGameHandler(PacketSession session, IMessage packet)
 	{
 		S_EnterGame enterGamePacket = packet as S_EnterGame;
 		ServerSession serverSession = session as ServerSession;
+		
+		Debug.Log("S_EnterGameHandler");
+		Debug.Log(enterGamePacket.Player);
+	}
+	
+	public static void S_LeaveGameHandler(PacketSession session, IMessage packet)
+	{
+		var gamePacket = packet as S_LeaveGame;
+		ServerSession serverSession = session as ServerSession;
+		
+		Debug.Log("S_LeaveGameHandler");
+	}
+	
+	public static void S_SpawnHandler(PacketSession session, IMessage packet)
+	{
+		var gamePacket = packet as S_Spawn;
+		ServerSession serverSession = session as ServerSession;
+		
+		Debug.Log("S_SpawnHandler");
+		Debug.Log(gamePacket.Players);
+	}
+	
+	public static void S_DespawnHandler(PacketSession session, IMessage packet)
+	{
+		var gamePacket = packet as S_Despawn;
+		ServerSession serverSession = session as ServerSession;
+		
+		Debug.Log("S_DespawnHandler");
+	}
+	
+	public static void S_MoveHandler(PacketSession session, IMessage packet)
+	{
+		var gamePacket = packet as S_Move;
+		ServerSession serverSession = session as ServerSession;
+		
+		Debug.Log("S_MoveHandler");
 	}
 }
---
+++ Server/Server/Game/GameRoom.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using Google.Protobuf.Protocol;
+
+namespace Server.Game;
+
+public class GameRoom
+{
+    object _lock = new object();
+    public int RoomId { get; set; }
+
+    List<Player> _players = new List<Player>();
+
+    public void EnterGame(Player newPlayer)
+    {
+        if (newPlayer == null)
+        {
+            return;
+        }
+
+        lock (_lock)
+        {
+            _players.Add(newPlayer);
+            newPlayer.Room = this;
+            
+            // 본인한테 정보 전송
+            {
+                S_EnterGame enterPacket = new S_EnterGame();
+                enterPacket.Player = newPlayer.Info;
+                newPlayer.Session.Send(enterPacket);
+                S_Spawn spawnPacket = new S_Spawn();
+                foreach (var player in _players)
+                {
+                    if (newPlayer != player)
+                    {
+                        spawnPacket.Players.Add(player.Info);
+                    }
+                }
+
+                newPlayer.Session.Send(spawnPacket);
+            }      
+            // 타인들한테 정보 전송
+            {
+                S_Spawn spawnPacket = new S_Spawn();
+                spawnPacket.Players.Add(newPlayer.Info);
+                foreach (var player in _players)
+                {
+                    if (newPlayer != player)
+                    {
+                        player.Session.Send(spawnPacket);
+                    }
+                }
+            }
+        }
+    }
+
+    public void LeaveGame(int playerId)
+    {
+        lock (_lock)
+        {
+            Player player = _players.Find(p =>
+            {
+                return p.Info.PlayerId == playerId;
+            });   
+            if (player == null)
+                return;
+
+            _players.Remove(player);
+            player.Room = null;
+
+            {
+                S_LeaveGame leavePk = new S_LeaveGame();
+                player.Session.Send(leavePk);
+            }
+
+            {
+                S_Despawn despawnPk = new S_Despawn();
+                despawnPk.PlayerIds.Add(playerId);
+                foreach (var p in _players)
+                {
+                    p.Session.Send(despawnPk);
+                }
+            }
+        }
+    }
+}
