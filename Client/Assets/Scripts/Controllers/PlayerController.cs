f5783f92692d23f1e1c7553fe849c682caeb0d3a
kimsy <kimsy@nhn.com>
2024-01-03 13:30:39 +0900

    멀티플레이 수업 구현

--- Client/Assets/Scripts/Controllers/PlayerController.cs
+++ Client/Assets/Scripts/Controllers/PlayerController.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Rendering;
 using UnityEngine;
 using static Define;
 
--- Server/Server/Session/ClientSession.cs
+++ Server/Server/Session/ClientSession.cs
@@ -1,61 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Net.Sockets;
-using System.Threading;
-using System.Threading.Tasks;
-using ServerCore;
 using System.Net;
-using Google.Protobuf.Protocol;
 using Google.Protobuf;
+using Google.Protobuf.Protocol;
+using ServerCore;
 
 namespace Server
 {
-	class ClientSession : PacketSession
-	{
-		public int SessionId { get; set; }
+    class ClientSession : PacketSession
+    {
+        public int SessionId { get; set; }
 
-		public override void OnConnected(EndPoint endPoint)
-		{
-			Console.WriteLine($"OnConnected : {endPoint}");
+        public void Send(IMessage packet)
+        {
+            string msgName = packet.Descriptor.Name.Replace("_", String.Empty);
+            MsgId msgId = (MsgId)Enum.Parse(typeof(MsgId), msgName);
+            ushort size = (ushort)packet.CalculateSize();
+            byte[] sendBuffer = new byte[size + 4];
+            Array.Copy(BitConverter.GetBytes(size + 4), 0, sendBuffer, 0, sizeof(ushort));
+            Array.Copy(BitConverter.GetBytes((ushort)msgId), 0, sendBuffer, 2, sizeof(ushort));
+            Array.Copy(packet.ToByteArray(), 0, sendBuffer, 4, size);
 
-			// PROTO Test
-			S_Chat chat = new S_Chat()
-			{
-				Context = "안녕하세요"
-			};
+            Send(new ArraySegment<byte>(sendBuffer));
+        }
 
-			ushort size = (ushort)chat.CalculateSize();
-			byte[] sendBuffer = new byte[size + 4];
-			Array.Copy(BitConverter.GetBytes(size + 4), 0, sendBuffer, 0, sizeof(ushort));
-			ushort protocolId = (ushort)MsgId.SChat;
-			Array.Copy(BitConverter.GetBytes(protocolId), 0, sendBuffer, 2, sizeof(ushort));
-			Array.Copy(chat.ToByteArray(), 0, sendBuffer, 4, size);
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected : {endPoint}");
 
-			Send(new ArraySegment<byte>(sendBuffer));
+            // PROTO Test
+            S_Chat chat = new S_Chat()
+            {
+                Context = "안녕하세요"
+            };
+            Send(chat);
+        }
 
-			//S_Chat chat2 = new S_Chat();
-			//chat2.MergeFrom(sendBuffer, 4, sendBuffer.Length - 4);
-			//////////////////////////
-			//////////////////////////
-			//Program.Room.Push(() => Program.Room.Enter(this));
-		}
+        public override void OnRecvPacket(ArraySegment<byte> buffer)
+        {
+            PacketManager.Instance.OnRecvPacket(this, buffer);
+        }
 
-		public override void OnRecvPacket(ArraySegment<byte> buffer)
-		{
-			PacketManager.Instance.OnRecvPacket(this, buffer);
-		}
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            SessionManager.Instance.Remove(this);
 
-		public override void OnDisconnected(EndPoint endPoint)
-		{
-			SessionManager.Instance.Remove(this);
+            Console.WriteLine($"OnDisconnected : {endPoint}");
+        }
 
-			Console.WriteLine($"OnDisconnected : {endPoint}");
-		}
-
-		public override void OnSend(int numOfBytes)
-		{
-			//Console.WriteLine($"Transferred bytes: {numOfBytes}");
-		}
-	}
+        public override void OnSend(int numOfBytes)
+        {
+            //Console.WriteLine($"Transferred bytes: {numOfBytes}");
+        }
+    }
 }
