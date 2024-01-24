f23dbe66cda64ca060e9a0f6944375e62d1948b5
kimsy <kimsy@nhn.com>
2024-01-24 13:02:21 +0900

    작업분 반영

---
+++ Server/Server/Game/Player.cs
@@ -0,0 +1,11 @@
+﻿using Google.Protobuf.Protocol;
+
+namespace Server.Game
+{
+    public class Player
+    {
+        public GameRoom Room { get; set; }
+        public ClientSession Session { get; set; }
+        public PlayerInfo Info { get; set; }
+    }
+}
---
+++ Server/Server/Game/RoomManager.cs
@@ -0,0 +1,7 @@
+﻿namespace Server.Game
+{
+    public class RoomManager
+    {
+        
+    }
+}
