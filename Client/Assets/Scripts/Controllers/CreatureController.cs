abfb080b3e686d67d1e5fc5681e9fde450b0bce5
kimsy <kimsy@nhn.com>
2024-07-02 21:19:11 +0900

    Hit 판정 #1, #2

--- Client/Assets/Scripts/Controllers/CreatureController.cs
+++ Client/Assets/Scripts/Controllers/CreatureController.cs
@@ -73,7 +73,6 @@ public class CreatureController : MonoBehaviour
         }
     }
 
-    protected MoveDir _lastDir = MoveDir.Down;
     public MoveDir Dir
     {
         get { return PosInfo.MoveDir; }
@@ -83,8 +82,6 @@ public class CreatureController : MonoBehaviour
                 return;
 
             PosInfo.MoveDir = value;
-            if (value != MoveDir.None)
-                _lastDir = value;
 
             UpdateAnimation();
             _updated = true;
@@ -99,24 +96,22 @@ public class CreatureController : MonoBehaviour
             return MoveDir.Left;
         else if (dir.y > 0)
             return MoveDir.Up;
-        else if (dir.y < 0)
-            return MoveDir.Down;
         else
-            return MoveDir.None;
+            return MoveDir.Down;
     }
 
     public Vector3Int GetFrontCellPos()
     {
         Vector3Int cellPos = CellPos;
 
-        switch (_lastDir)
+        switch (Dir)
         {
             case MoveDir.Up:
                 cellPos += Vector3Int.up;
                 break;
             case MoveDir.Down:
                 cellPos += Vector3Int.down;
-                break;
+                break; 
             case MoveDir.Left:
                 cellPos += Vector3Int.left;
                 break;
@@ -132,7 +127,7 @@ public class CreatureController : MonoBehaviour
     {
         if (State == CreatureState.Idle)
         {
-            switch (_lastDir)
+            switch (Dir)
             {
                 case MoveDir.Up:
                     _animator.Play("IDLE_BACK");
@@ -176,7 +171,7 @@ public class CreatureController : MonoBehaviour
         }
         else if (State == CreatureState.Skill)
         {
-            switch (_lastDir)
+            switch (Dir)
             {
                 case MoveDir.Up:
                     _animator.Play("ATTACK_BACK");
@@ -220,7 +215,7 @@ public class CreatureController : MonoBehaviour
         transform.position = pos;
 
         State = CreatureState.Idle;
-        Dir = MoveDir.None;
+        Dir = MoveDir.Down;
         UpdateAnimation();
     }
 
--- Server/Server/Game/GameRoom.cs
+++ Server/Server/Game/GameRoom.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Google.Protobuf;
 using Google.Protobuf.Protocol;
 
@@ -9,7 +10,13 @@ namespace Server.Game
         private object _lock = new object();
         public int RoomId { get; set; }
 
-        private List<Player> _players = new List<Player>();
+        Dictionary<int, Player> _players = new Dictionary<int, Player>();
+        Map _map = new Map();
+
+        public void Init(int mapId)
+        {
+            _map.LoadMap(mapId);
+        }
 
         public void EnterGame(Player newPlayer)
         {
@@ -18,7 +25,7 @@ namespace Server.Game
 
             lock (_lock)
             {
-                _players.Add(newPlayer);
+                _players.Add(newPlayer.Info.PlayerId, newPlayer);
                 newPlayer.Room = this;
 
                 // 본인한테 정보 전송
@@ -28,7 +35,7 @@ namespace Server.Game
                     newPlayer.Session.Send(enterPacket);
 
                     S_Spawn spawnPacket = new S_Spawn();
-                    foreach (Player p in _players)
+                    foreach (Player p in _players.Values)
                     {
                         if (newPlayer != p)
                         {
@@ -42,7 +49,7 @@ namespace Server.Game
                 {
                     S_Spawn spawnPacket = new S_Spawn();
                     spawnPacket.Players.Add(newPlayer.Info);
-                    foreach (Player p in _players)
+                    foreach (Player p in _players.Values)
                     {
                         if (newPlayer != p)
                         {
@@ -57,11 +64,10 @@ namespace Server.Game
         {
             lock (_lock)
             {
-                Player player = _players.Find(p => p.Info.PlayerId == playerId);
-                if (player == null)
+                Player player = null;
+                if (_players.Remove(playerId, out player) == false)
                     return;
 
-                _players.Remove(player);
                 player.Room = null;
 
                 // 본인한테 정보 전송
@@ -74,7 +80,7 @@ namespace Server.Game
                 {
                     S_Despawn deespawnPacket = new S_Despawn();
                     deespawnPacket.PlayerIds.Add(playerId);
-                    foreach (Player p in _players)
+                    foreach (Player p in _players.Values)
                     {
                         if (player != p)
                             p.Session.Send(deespawnPacket);
@@ -90,9 +96,20 @@ namespace Server.Game
 
             lock (_lock)
             {
-                // 일단 서버에서 좌표 이동
+                // TODO: 검증
+                PositionInfo movePosInfo = movePacket.PosInfo;
                 PlayerInfo info = player.Info;
-                info.PosInfo = movePacket.PosInfo;
+
+                // 다른 좌표로 이동할 경우, 갈 수 있는지 체크
+                if (movePosInfo.PosX != info.PosInfo.PosX || movePosInfo.PosY != info.PosInfo.PosY)
+                {
+                    if (_map.CanGo(new Vector2Int(movePosInfo.PosX, movePosInfo.PosY)) == false)
+                        return;
+                }
+
+                info.PosInfo.State = movePosInfo.State;
+                info.PosInfo.MoveDir = movePosInfo.MoveDir;
+                _map.ApplyMove(player, new Vector2Int(movePosInfo.PosX, movePosInfo.PosY));
 
                 // 다른 플레이어한테도 알려준다
                 S_Move resMovePacket = new S_Move();
@@ -124,6 +141,12 @@ namespace Server.Game
                 Broadcast(skill);
 
                 // TODO: 데미지 판정
+                Vector2Int skillPos = player.GetFrontCellPos(info.PosInfo.MoveDir);
+                Player target = _map.Find(skillPos);
+                if (target != null)
+                {
+                    Console.WriteLine("Hit Player !");
+                }
             }
         }
 
@@ -131,7 +154,7 @@ namespace Server.Game
         {
             lock (_lock)
             {
-                foreach (Player p in _players)
+                foreach (Player p in _players.Values)
                 {
                     p.Session.Send(packet);
                 }
